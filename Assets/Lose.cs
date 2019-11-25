053c4eb98e4762468ca312880b0f2b46b2507802
Piotr Waśk <contact@codeagency.pl>
2019-11-25 09:15:49 +0100

    Initial commit

---
+++ Assets/Lose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour
+{
+    public Text info;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int points = Engine.point;
+        Debug.Log(points);
+        info.text = "Koniec gry\nZdobyte punkty: " + points.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+}
